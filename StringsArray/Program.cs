6225918064b43f2fc1c192084f4da98502748597
Алексей <insert159@mail.ru>
2022-10-02 14:25:04 +0600

    half of task

--- StringsArray/Program.cs
+++ StringsArray/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String[] arr = new String[5];
+
+void showSmallArr()
+{
+    Console.WriteLine("Введите 5 слов, разделяя каждый нажанием Enter");
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Console.ReadLine();
+    }
+    Console.WriteLine("Массив заполнен");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+        Console.WriteLine("Массив с тремя и менее символами:");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
+
+showSmallArr();
